6bfc09627f7673d7ac0bc0ab55f1f10e06a81855
Override <122590935+TuringProblem@users.noreply.github.com>
2026-08-08 13:58:17 -0400

    feat: started some structure

--- src/menu.hpp
+++ src/menu.hpp
@@ -15,21 +15,29 @@
  **/
 enum class MenuState {
 
-}
+};
+const auto lineCreator = [](const std::string &word) {
+  size_t size = word.size();
+  for (size_t i = 0; i < size; i++) {
+    std::cout << "-";
+  }
 
-void displayMainScreen(const int &num) {
-  const auto lineCreator = [&](const std::string &word) {
-    size_t size = word.size();
-    for (size_t i = 0; i < size; i++) {
-      std::cout << "-";
-    }
+  std::cout << "\n" << word << "\n";
 
-    std::cout << "\n" << word << "\n";
+  for (size_t i = 0; i < size; i++) {
+    std::cout << "-";
+  }
+  std::cout << "\n";
+};
 
-    for (size_t i = 0; i < size; i++) {
-      std::cout << "-";
-    }
-  };
+void screenHandler(const int &num) {
+  switch (num) {
 
-  lineCreator("Welcome to PUNCH menu!");
+  case 1:
+    lineCreator("Welcome to PUNCH menu!");
+    break;
+
+  default:
+    break;
+  }
 }
